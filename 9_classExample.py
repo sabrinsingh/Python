642bc6008eeb36ad4c571e23521e698f8c448bfb
sabrin.singh <you@example.com>
2020-08-31 22:49:06 +0545

    checked

--- 9_classExample.py
+++ 9_classExample.py
@@ -1,4 +1,5 @@
 class myClass():
+
     def method1(self):
         print("First method")
     
@@ -12,15 +13,33 @@ class myAnotherClass():
     def method2(self, str):
         print("self is similar to \"this\" and this is the second string from myAnotherClass :" + str)
 
+    
+
+class otherClass():
+    def __init__(self, firstname, middlename, lastname):
+        self.firstname=firstname
+        self.middlename=middlename
+        self.lastname=lastname
+    
+    def name(self):
+        print(self.firstname, self.middlename,self.lastname)
 
 def main():
-    c=myClass()
-    c.method1()
-    c.method2("HAHAHA")
+    obj1=myClass()
+    obj1.method1()
+    obj1.method2("HAHAHA")
 
-    c=myAnotherClass()
-    c.method1()
-    c.method2("HEEEEEHHHHHHAWWWWWWWWWWWWWw")
+    obj2=myAnotherClass()
+    obj2.method1()
+    obj2.method2("HEEEEEHHHHHHAWWWWWWWWWWWWWw")
+
+    obj3=otherClass(
+        'Sabrin',
+        'Lal',
+        'Singh'
+    )
+
+    obj3.name()
 
 if __name__=="__main__":
     main()
